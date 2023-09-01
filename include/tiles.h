beecfccab464a3f66c2eaa3494db0fa3b15366ee
Lukas Krickl <lukas@krickl.dev>
2023-09-01 06:15:00 +0200

    WIP: Tiles

--- include/tiles.h
+++ include/tiles.h
@@ -4,9 +4,23 @@
 #include <stdint.h>
 
 #define MRG_TILE_TABLE_LEN 256
+#define MRG_TILE_STATIC_LEN 128
 
-extern uint8_t *MRG_TILE_TABLE[MRG_TILE_TABLE_LEN];
+enum mrg_tilesets {
+  MRG_TILESET_STATIC,
+  MRG_TILESET_DEBUG
+};
 
-extern uint8_t MRG_TEST_TILE[8];
+struct mrg_tileset {
+  void *static_tiles;
+  void *dynamic_tiles;
+};
+
+// load static tiles
+struct mrg_tileset mrg_tile_setinit(void);
+
+int mrg_tile_set_load(enum mrg_tilesets set); 
+
+void mrg_tile_setfree(struct mrg_tileset *ts);
 
 #endif
