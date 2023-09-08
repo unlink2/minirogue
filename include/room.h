fb667cdd833ef0550fc917bb3c7f3f722abcc52c
Lukas Krickl <lukas@krickl.dev>
2023-09-08 17:56:38 +0200

    WIP: tile table file system loader

--- include/room.h
+++ include/room.h
@@ -2,6 +2,9 @@
 #define ROOM_H_
 
 #include <stddef.h>
+#include <stdint.h>
+
+#define MRG_ROOMS_MAX 64
 
 /**
  * A room is an abstract representation of
@@ -9,11 +12,19 @@
  * and it can be drawn onto the map
  */
 struct mrg_room {
+  // TODO: tile data go here
+  int8_t tiles;
+  int8_t flags;
+};
+
+struct mrg_room_instacne {
   // room handles for all directions
   int room_west;
   int room_east;
   int room_north;
   int room_south;
+
+  int room_instance;
 };
 
 struct mrg_room_graph {
@@ -24,6 +35,7 @@ struct mrg_room_graph {
 // collection of all room templates
 struct mrg_room_tbl {
   // TODO: how do we store rooms?
+  struct mrg_room rooms[MRG_ROOMS_MAX];
   size_t len;
 };
 
