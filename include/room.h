e20a2faebb1e6c83f8c7e29f9fd74941d64e6b43
Lukas Krickl <lukas@krickl.dev>
2023-09-08 10:10:39 +0200

    WIP: Room graph

--- include/room.h
+++ include/room.h
@@ -1,12 +1,30 @@
 #ifndef ROOM_H_
 #define ROOM_H_
 
+#include <stddef.h>
+
 /**
- * A room is an abstract representation of 
- * a walkable area. It connects to other rooms in a graph 
+ * A room is an abstract representation of
+ * a walkable area. It connects to other rooms in a graph
  * and it can be drawn onto the map
  */
 struct mrg_room {
+  // room handles for all directions
+  int room_west;
+  int room_east;
+  int room_north;
+  int room_south;
 };
 
-#endif 
+struct mrg_room_graph {
+  struct mrg_room *rooms;
+  size_t room_len;
+};
+
+// collection of all room templates
+struct mrg_room_tbl {
+  // TODO: how do we store rooms?
+  size_t len;
+};
+
+#endif
