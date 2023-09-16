10a8607710b98f981e67e441ba973283482c3e5e
Lukas Krickl <lukas@krickl.dev>
2023-09-16 13:36:29 +0200

    WIP: Command exec system

--- src/command.c
+++ src/command.c
@@ -1,4 +1,5 @@
 #include "command.h"
+#include <string.h>
 
 int mrg_cmd_exec(void *fp, mrg_fputs puts, const char *args,
                  const struct mrg_cmd *tbl) {
@@ -8,7 +9,28 @@ int mrg_cmd_exec(void *fp, mrg_fputs puts, const char *args,
 }
 
 int mrg_cmd_help(void *fp, mrg_fputs puts, struct mrg_cmd *cmd,
-                 const char *args, const struct mrg_cmd *tbl) {}
+                 const char *args, const struct mrg_cmd *tbl) {
+  const size_t buffer_len = 4096;
+  char buffer[buffer_len];
+  while (tbl->name) {
+    memset(buffer, 0, buffer_len);
+    strncat(buffer, tbl->name, buffer_len);
+    strncat(buffer, " ", buffer_len);
+
+    struct mrg_arg *args = tbl->args;
+    while (args && args->name) {
+      strncat(buffer, args->name, buffer_len);
+      strncat(buffer, " ", buffer_len);
+      args++;
+    }
+
+    puts(buffer, fp);
+
+    tbl++;
+  }
+
+  return 0;
+}
 
 const struct mrg_cmd mrg_cmd_tbl[] = {
     {"help", "Display this help message", mrg_cmd_help, NULL}};
