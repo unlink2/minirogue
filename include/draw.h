6ec07d77e870ae36ec745a63731639b750c8ca04
Lukas Krickl <lukas@krickl.dev>
2023-09-26 07:54:44 +0200

    Increased color palette to the full 8 colors

--- include/draw.h
+++ include/draw.h
@@ -22,5 +22,13 @@ struct mrg_color {
   (struct mrg_color) { 0x49, 0x6A, 0xB4, 0xFF }
 #define MRG_COLOR3                                                             \
   (struct mrg_color) { 0xC9, 0x00, 0x00, 0xFF }
+#define MRG_COLOR4                                                             \
+  (struct mrg_color) { 0xA7, 0x62, 0x28, 0xFF }
+#define MRG_COLOR5                                                             \
+  (struct mrg_color) { 0x5D, 0xA9, 0x3F, 0xFF }
+#define MRG_COLOR6                                                             \
+  (struct mrg_color) { 0x9F, 0x9D, 0x88, 0xFF }
+#define MRG_COLOR7                                                             \
+  (struct mrg_color) { 0xF5, 0xBE, 0x4B, 0xFF }
 
 #endif
