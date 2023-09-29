1e4f91c28440c2d78f4e82cf6419380bde6cd45d
Lukas Krickl <lukas@krickl.dev>
2023-09-29 07:39:03 +0200

    WIP: Entity spawning from template code

--- src/entity.c
+++ src/entity.c
@@ -18,6 +18,25 @@ int mrg_beh_nop(struct mrg_state *state, struct mrg_entity *entity) {
   return 0;
 }
 
+int mrg_entity_spawn(struct mrg_state *state, struct mrg_entity *entity) {
+  struct mrg_entity_tbl *tbl = &state->entity_tbl;
+  int handle = mrg_entity_alloc(tbl);
+
+  if (handle == -1) {
+    return -1;
+  }
+
+  struct mrg_entity *e = &tbl->slots[handle];
+  mrg_entity_init_type(e, entity->type);
+  e->type = entity->type;
+  e->flags = entity->flags | e->flags;
+  e->x = entity->x;
+  e->y = entity->y;
+  e->tileset_id = entity->tileset_id;
+
+  return 0;
+}
+
 // TODO: this is pretty terrble, but also a nice and easy way for now...
 // implement better solution when generalizing for other entities
 void mrg_beh_move_col(struct mrg_state *state, struct mrg_entity *entity,
@@ -124,34 +143,24 @@ int mrg_entity_init(struct mrg_entity *entity) {
 }
 
 int mrg_entities_from_idc(struct mrg_state *state, struct mrg_idc_file *f) {
-  struct mrg_entity_tbl *tbl = &state->entity_tbl;
-
   for (size_t i = 0; i < f->header.n_entries; i++) {
     struct mrg_idc_dir *dir = &f->dirs[i];
-    if (!(dir->type & MRG_IDC_DIR_ENTITY) &&
-        dir->entry.entity.room_id == state->map.room->room_id) {
+    if (!(dir->type & MRG_IDC_DIR_ENTITY) ||
+        dir->entry.entity.room_id != state->map.room->room_id) {
       continue;
     }
-
-    int handle = mrg_entity_alloc(tbl);
-    if (handle == -1) {
-      return -1;
-    }
-
-    // TODO: handle room id
-    struct mrg_entity *e = &tbl->slots[handle];
-    mrg_entity_init_type(e, dir->entry.entity.type);
-    e->type = dir->entry.entity.type;
-    e->flags = dir->entry.entity.flags | e->flags;
-    e->x = dir->entry.entity.x;
-    e->y = dir->entry.entity.y;
 
     char tile_path[MRG_IDC_FILE_NAME_LEN + 1];
     memset(tile_path, 0, MRG_IDC_FILE_NAME_LEN + 1);
     memcpy(tile_path, dir->entry.room.tile_set, MRG_IDC_FILE_NAME_LEN);
 
-    e->tileset_id = mrg_tile_set_load(&state->tile_tbl, state->platform,
-                                      tile_path, state->tile_w, state->tile_h);
+    dir->entry.entity.tileset_id =
+        mrg_tile_set_load(&state->tile_tbl, state->platform, tile_path,
+                          state->tile_w, state->tile_h);
+
+    if (mrg_entity_spawn(state, &dir->entry.entity) == -1) {
+      return -1;
+    }
   }
 
   return 0;
